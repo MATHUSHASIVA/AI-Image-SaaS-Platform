7a035ea66717d6eb1718b77a17b56dfb2fc96cfb
MATHUSHASIVA <mathushasiva2k@gmail.com>
2025-09-12 15:15:29 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
-x
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/api/webhooks/clerk',
+  '/api/webhooks/stripe'
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
